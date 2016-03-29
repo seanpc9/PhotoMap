3b4d88bf1f4b2e3a37e1df1cafe3dffc2b8d8eba
Sean Crenshaw <seanpc9@vt.edu>
2016-03-29 19:50:44 -0400

    Merge TableView into main screen and show nearby locations

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 import MapKit
 
-class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, LocationsViewControllerDelegate, MKMapViewDelegate {
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, LocationsViewControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
+    var curImage: UIImage!
     
     @IBOutlet weak var mapView: MKMapView!
-    var curImage: UIImage!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let CLIENT_ID = "QA1L0Z0ZNA2QVEEDHFPQWK0I5F1DE3GPLSNW4BZEBGJXUCFL"
+    let CLIENT_SECRET = "W2AOE1TYC4MHK5SZYOUGX0J3LVRALMPB4CXT3ZH21ZCPUMCU"
+    
+    var results: NSArray = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +29,14 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         let burgRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.2289788, -80.4258549),MKCoordinateSpanMake(0.1, 0.1))
         mapView.setRegion(burgRegion, animated: false)
         mapView.delegate = self
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        searchBar.delegate = self
+        
+        searchBar.placeholder = "Basketball"
+        
+        fetchNear()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +50,8 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
         
         // Dismiss UIImagePickerController to go back to your original view controller
+        
+        // curImage = editedImage
         dismissViewControllerAnimated(true, completion: nil)
         performSegueWithIdentifier("tagSegue", sender: editedImage)
     }
@@ -100,6 +118,115 @@ class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate,
         annotationView!.image = thumbnail
         
         return annotationView
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("LocationCell") as! LocationCell
+        
+        cell.location = results[indexPath.row] as! NSDictionary
+        
+        return cell
+    }
+    
+    func searchBar(searchBar: UISearchBar, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        let newText = NSString(string: searchBar.text!).stringByReplacingCharactersInRange(range, withString: text)
+        fetchLocations(newText)
+        
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        fetchLocations(searchBar.text!)
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        self.searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+    
+    func fetchNear(near: String = "Blacksburg, VA") {        // Let user input String for near
+        let baseUrlString = "https://api.foursquare.com/v2/venues/search?"
+        let queryString = "client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=20141020&near=\(near)"
+        
+        let url = NSURL(string: baseUrlString + queryString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)!
+        let request = NSURLRequest(URL: url)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                    data, options:[]) as? NSDictionary {
+                    NSLog("response: \(responseDictionary)")
+                    let test = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                    if (test.count != 0) {
+                        self.results = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        });
+        task.resume()
+    }
+    
+    func fetchLocations(query: String, near: String = "Blacksburg, VA") {        // Let user input String for near
+        let baseUrlString = "https://api.foursquare.com/v2/venues/search?"
+        let queryString = "client_id=\(CLIENT_ID)&client_secret=\(CLIENT_SECRET)&v=20141020&near=\(near)&query=\(query)"
+        
+        let url = NSURL(string: baseUrlString + queryString.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())!)!
+        let request = NSURLRequest(URL: url)
+        
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: { (dataOrNil, response, error) in
+            if let data = dataOrNil {
+                if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                    data, options:[]) as? NSDictionary {
+                    NSLog("response: \(responseDictionary)")
+                    let test = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                    if (test.count != 0) {
+                        self.results = responseDictionary.valueForKeyPath("response.venues") as! NSArray
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        });
+        task.resume()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        let vc = LocationsViewController()
+        vc.delegate = self
+        
+        let venue = results[indexPath.row] as! NSDictionary
+        
+        let lat = venue.valueForKeyPath("location.lat") as! NSNumber
+        let lng = venue.valueForKeyPath("location.lng") as! NSNumber
+        
+        let latString = "\(lat)"
+        let lngString = "\(lng)"
+        
+        print(latString + " " + lngString)
+        
+        locationsPickedLocation(vc, latitude: lat, longitude: lng)
     }
 
     // MARK: - Navigation
